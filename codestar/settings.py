604e0559239d4894156054ccba77f987119d7d9e
Mohammad Butt <u.butt91@gmail.com>
2024-06-17 01:32:21 +0000

    Add a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -90,6 +90,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.8000-usmanbutt1-djangoblog-jwylu1uvyoj.ws.codeinstitute-ide.net",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
