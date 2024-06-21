935924937d0a22e1aa568b5f76a3ab40abead74d
Mohammad Butt <u.butt91@gmail.com>
2024-06-21 14:23:45 +0000

    Generic post view

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
     
@@ -29,3 +30,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
